5eb263b8032c25148a47bb0685194157dd53f383
Evan Sosenko <evan.sosenko@meltwater.com>
2018-02-17 17:59:35 -0800

    Add snapshot server tests

---
+++ test/server/health.test.js
@@ -0,0 +1,14 @@
+import test from 'ava'
+import { httpGetJson } from '@meltwater/mlabs-koa'
+
+import { loadConfig } from '../../server/boot'
+
+test.beforeEach(async t => {
+  const config = await loadConfig()
+  t.context.api = `http://localhost:${config.get('port')}`
+})
+
+test('get health', async t => {
+  const res = await httpGetJson(`${t.context.api}/health`)
+  t.snapshot(res)
+})
