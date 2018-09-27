bbd7c750a77c3b6474d0a462435e9e1912f2b3fe
Evan Sosenko <evan.sosenko@meltwater.com>
2018-09-27 15:01:58 -0700

    Update to mlabs-koa v6

--- examples/health.js
+++ examples/health.js
@@ -6,7 +6,7 @@ export default ({ log, koaApi }) => async (check = '') => {
   const url = new URL(`${koaApi}/health/${check}`)
   const { healthy, error } = await httpGetJson({
     hostname: url.hostname,
-    path: url.path,
+    path: url.pathname,
     port: url.port,
     headers: { accept: 'application/json' }
   })
