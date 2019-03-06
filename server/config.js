a7e4aca829b5fc033a47a87f3a4898bb7bf04b10
Evan Sosenko <evan.sosenko@meltwater.com>
2019-03-06 11:20:59 -0800

    Add logRedaction config option

--- server/config.js
+++ server/config.js
@@ -1,11 +1,20 @@
 import path from 'path'
+import { promisify } from 'util'
 
 export default async (
   configFactory,
   /* istanbul ignore next */
   root = '../'
 ) => {
+  const config = await getConfig(configFactory)
   const dataPath = path.resolve(__dirname, root, 'data')
+  const logRedaction = config.get('logRedaction') || {}
+
+  configFactory.addOverride({
+    log: {
+      redact: { paths: [].concat.apply([], Object.values(logRedaction)) }
+    }
+  })
 
   configFactory.addOverride({
     data: {
@@ -21,3 +30,7 @@ export default async (
     }
   })
 }
+
+const getConfig = configFactory => promisify(
+  (...args) => configFactory.create(...args)
+)()
